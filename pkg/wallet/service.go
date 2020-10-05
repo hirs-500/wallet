f8a2d31eb49b84037320c9c32312198e27c9d1ca
hirs_2004 <hirs_2004@mail.ru>
2020-10-05 14:47:33 +0500

    изменено функция paymentfind

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -118,12 +118,13 @@ func (s *Service)FindPaymentByID (paymentID string) (*types.Payment, error ) {
 			payment = pay
                break
 		}
+	}
 	if payment == nil {
 		return nil, ErrPaymentNotFound
 
 	}
 		
-	}
+	
 	return payment, nil
 }
 //Reject отмена платежа
