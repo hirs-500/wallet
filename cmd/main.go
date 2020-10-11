3bd02341fe21338e7d48e1465e4888932ee55a78
hirs_2004 <hirs_2004@mail.ru>
2020-10-11 20:11:43 +0500

    работа с файлами

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/hirs-500/wallet/pkg/wallet"
+)
+
+
+
+
+func main ()  {
+svc := &wallet.Service{}
+svc.RegisterAccount("+22665225")
+svc.RegisterAccount("+12665225")
+svc.RegisterAccount("+02665225")
+svc.ExpotToFile("data/export")
+}
